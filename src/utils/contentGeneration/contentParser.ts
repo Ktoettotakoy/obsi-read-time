ab09a43ad7e1898b8d047a4196af3612a06fc78c
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2025-08-05 12:42:53 +0100

    added setting to change the pattern for TOC to any you like

--- src/utils/contentGeneration/contentParser.ts
+++ src/utils/contentGeneration/contentParser.ts
@@ -1,7 +1,7 @@
 // return index of a line that contains the ## Content
-function findContentsLine(fileContent: string): number {
+function findContentsLine(fileContent: string, toc_pattern: string): number {
     const lines = fileContent.split("\n");
-    return lines.findIndex(line => line.trim() === "## Content:");
+    return lines.findIndex(line => line.trim() === toc_pattern);
 }
 
 function findThreeDashesAfterContents(fileContent:string, contentIndex: number): number {
@@ -9,7 +9,7 @@ function findThreeDashesAfterContents(fileContent:string, contentIndex: number):
     return lines.findIndex((line, i) => i > contentIndex && line.trim() === "---");
 }
 
-function findAllHeadingsInOrder(fileContent: string): string[] {
+function findAllHeadingsInOrder(fileContent: string, toc_pattern_to_skip: string): string[] {
     const headingRegex = /^(#{1,6})\s+(.*)$/;
 
     const lines = fileContent.split("\n");
@@ -19,7 +19,7 @@ function findAllHeadingsInOrder(fileContent: string): string[] {
     let codeComment = false;
     for (let i = 0; i < lines.length; i++) {
         const match = lines[i].match(headingRegex);
-        if (match && lines[i].trim() !== "## Content:" && !codeComment) {
+        if (match && lines[i].trim() !== toc_pattern_to_skip && !codeComment) {
             headings.push(lines[i].trim());
         }
         // filter out code comments
